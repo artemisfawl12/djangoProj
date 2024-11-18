a0a45ac3e65b2d4e3b5d4ef1af4c9f62dcfc7aa7
artemisfawl12 <artemisfawl@naver.com>
2024-11-18 23:27:42 +0900

    다시 제목 변경

--- counter/views.py
+++ counter/views.py
@@ -2,8 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 def count_characters(request):
     if request.method == "POST":
-        name =request.POST.get('name','')
-        char_count=name+"은 바보입니다"
-        return render(request,'counter/result.html',{'name':name,'char_count':char_count})
+        ticker =request.POST.get('ticker','')
+        start_date = request.POST.get('start_date', '')
+        end_date = request.POST.get('end_date', '')
+        buy_strat = request.POST.get('buy_strat', '')
+
+        buy_navigate="매수전략은 다음과 같습니다: "+buy_strat
+        return render(request,'counter/result.html',{'ticker':ticker,'buy_navigate':buy_navigate})
 
     return render(request,'counter/form.html')
