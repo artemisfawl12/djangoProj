0e4855f42a5bfad6be048e196a277d784a20c4e8
artemisfawl12 <artemisfawl@naver.com>
2024-11-18 22:57:09 +0900

    STATIC ROOT 생성

--- counter/views.py
+++ counter/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 def count_characters(request):
     if request.method == "POST":
         name =request.POST.get('name','')
-        char_count=len(name)
+        char_count=name+"은 바보입니다"
         return render(request,'counter/result.html',{'name':name,'char_count':char_count})
 
     return render(request,'counter/form.html')
