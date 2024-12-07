cb6033d403b855d9b2c53cdb1a926ea6abcfd646
artemisfawl12 <artemisfawl@naver.com>
2024-12-08 07:00:38 +0900

    null 허용

--- counter/models.py
+++ counter/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class FileLog(models.Model):
-    ip_address=models.GenericIPAddressField(default='0.0.0.0') #default 추가
-    timestamp=models.DateTimeField(auto_now_add=True)
-    status=models.CharField(max_length=50)
+    ip_address=models.GenericIPAddressField(default='0.0.0.0',null=True, blank=True) #default 추가
+    timestamp=models.DateTimeField(auto_now_add=True,null=True, blank=True)
+    status=models.CharField(max_length=50,null=True, blank=True)
 
     def __str__(self):
         return f"{self.ip_address} - {self.status} at {self.timestamp}"
